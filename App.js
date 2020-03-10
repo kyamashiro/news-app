5591357f91022985d696c38e02a14f98ca49fb93
kyamashiro <kyamashiro73@gmail.com>
2020-03-10 22:06:25 +0900

    Section4

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}></View>
-        <View style={styles.rightContainer}></View>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: "https://picsum.photos/200/300" }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3} style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <Text style={styles.subText}>ReactNews</Text>
+        </View>
       </View>
     </View>
   );
@@ -27,11 +43,19 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   leftContainer: {
-    backgroundColor: "red",
     width: 100,
   },
   rightContainer: {
-    backgroundColor: "blue",
     flex: 1,
+    flexDirection: "column",
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray",
   },
 });
