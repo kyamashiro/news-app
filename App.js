41ec10fa590aedb4dcf61a3bfbb3dd49c18ebaad
kyamashiro <kyamashiro73@gmail.com>
2020-04-05 15:34:41 +0900

    useStateとuseEffect

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Platform, FlatList, SafeAreaView } from "react-native";
 import ListItem from "./components/ListItem";
-import articles from "./dummies/articles";
+import dummyArticles from "./dummies/articles";
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    // mount時に呼び出される
+    // alert("called!");
+    // 2秒後にdummyデータが渡される
+    const timer = setTimeout(() => {
+      setArticles(dummyArticles);
+    }, 2000);
+    // アンマウント時にcleanupが実行される
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
