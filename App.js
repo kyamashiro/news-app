d797c695e3ee51026cea1f11fb5544304a8463ea
kyamashiro <yamashiro@rrr-z.co.jp>
2020-04-06 12:07:13 +0900

    React Navigationの実装

--- App.js
+++ App.js
@@ -1,47 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Platform, FlatList, SafeAreaView } from "react-native";
-import ListItem from "./components/ListItem";
-import axios from "axios";
-// @ts-ignore
-import { API_KEY } from "react-native-dotenv";
-const URL = `http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${API_KEY}`;
+import React from "react";
+import AppNavigator from "./navigation/AppNavigator";
 
 export default function App() {
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === "android" ? 25 : 0,
-    backgroundColor: "#fff",
-  },
-});
---
+++ navigation/AppNavigator.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+export default AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
