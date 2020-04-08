fcc8afb0394c8a6537fb32ea8fd58cbda49fb5a5
kyamashiro <kyamashiro73@gmail.com>
2020-04-08 22:21:01 +0900

    Tabにアイコンを追加

--- navigation/AppNavigator.jsx
+++ navigation/AppNavigator.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import ArticleScreen from "../screens/ArticleScreen";
 import ClipScreen from "../screens/ClipScreen";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -12,8 +13,16 @@ const Tab = createBottomTabNavigator();
 const HomeStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-      <Stack.Screen name="Article" component={ArticleScreen} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Article"
+        component={ArticleScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
@@ -21,15 +30,32 @@ const HomeStack = () => {
 const ClipStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Clip" component={ClipScreen} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Clip"
+        component={ClipScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
 
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+
+    if (route.name === "Home") {
+      iconName = "home";
+    } else if (route.name === "Clip") {
+      iconName = "bookmark";
+    }
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  },
+});
+
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
         <Tab.Screen name="Home" component={HomeStack} />
         <Tab.Screen name="Clip" component={ClipStack} />
       </Tab.Navigator>
